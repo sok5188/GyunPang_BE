9d805797d8cdfae0e380571a628075dbfe7a8c4c
sok5188 <sok5188@gmail.com>
2024-04-10 18:18:55 +0900

    add auth test api

--- src/main/java/com/gyunpang/be/controller/HealthController.java
+++ src/main/java/com/gyunpang/be/controller/HealthController.java
@@ -1,11 +1,14 @@
 package com.gyunpang.be.controller;
 
+import java.util.Enumeration;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -21,5 +24,20 @@ public class HealthController {
 			return ResponseEntity.ok(customEnv + " hello");
 		}
 		else return ResponseEntity.ok("Cannot find color");
+	}
+	@GetMapping("/open/123")
+	public ResponseEntity tst1(){
+		return ResponseEntity.ok("openAPITEST");
+	}
+
+	@GetMapping("/authTest")
+	public ResponseEntity authTest(HttpServletRequest request){
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()){
+			String key = headerNames.nextElement();
+			String value = request.getHeader(key);
+			System.out.println("key = " + key+" / val : "+ value);
+		}
+		return ResponseEntity.ok("AUTH_TEST");
 	}
 }
