16c89edcf749f796c60591eede1fb6c7aa488522
sok5188 <sok5188@gmail.com>
2024-08-12 20:57:43 +0900

    [feat] add kafka

---
+++ src/main/java/com/gyunpang/be/service/KafkaConsumeService.java
@@ -0,0 +1,22 @@
+package com.gyunpang.be.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaConsumeService {
+	@KafkaListener(topics = "${kafka.topic.log}",groupId = "be")
+	public void consumeString(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION) String partition){
+		log.info("[consumer] msg:{} from partition {}",message,partition);
+	}
+}
