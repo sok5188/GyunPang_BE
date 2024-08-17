220d8b27cfb69cb8253f71d6748045b2d1b32e44
sok5188 <sok5188@gmail.com>
2024-08-17 15:31:27 +0900

    [fix] modify variable name PINPOINT_AGENTID -> PINPOINT_AGENT_ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 java -jar -javaagent:/pinpoint-agent/pinpoint-bootstrap-3.0.0.jar \
-  -Dpinpoint.agentId=${PINPOINT_AGENTID:-NoAgentId} \
+  -Dpinpoint.agentId=${PINPOINT_AGENT_ID:-NoAgentId} \
   -Dpinpoint.applicationName=Main-Backend \
   app.jar
