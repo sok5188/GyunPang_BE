7857e3a3180a3d72b6ba07e8d7fec465a167894c
sok5188 <sok5188@gmail.com>
2025-01-22 16:09:38 +0900

    [fix] remove security dependency and use BCrypt directly

--- src/main/java/com/gyunpang/be/common/util/CommonUtil.java
+++ src/main/java/com/gyunpang/be/common/util/CommonUtil.java
@@ -2,16 +2,19 @@ package com.gyunpang.be.common.util;
 
 import java.util.UUID;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class CommonUtil {
+
 	public static String passwordEncoder(String plainPassword) {
-		//TODO : Need to Encrypt ..
-		String encryptedPassword = plainPassword + "enc";
-		return encryptedPassword;
+		return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+	}
+
+	public static boolean passwordMatches(String plainPassword, String hashedPassword) {
+		return BCrypt.checkpw(plainPassword, hashedPassword); // 비밀번호 검증
 	}
 
 	public static String makeRandomPassword() {
-		//TODO : Need to make some encrypt
-		return UUID.randomUUID().toString();
+		return UUID.randomUUID().toString().replace("-", "").substring(0, 12);
 	}
-
 }
